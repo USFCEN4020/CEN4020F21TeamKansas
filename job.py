fb7c587a2cfbe4519bb892306347f5ee6a6ce7d5
Jason Truong <nghiatruong@usf.edu>
2021-10-31 21:31:19 -0400

    fixed conflict

--- job.py
+++ job.py
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-class Job:
-    def __init__(self, title, description, employer, location, salary, post_name):
-        self.title = title
-        self.description = description
-        self.employer = employer
-        self.location = location
-        self.salary = salary
-        self.post_name = post_name
-
-    def get_title(self):
-        return self.title
-
-    def get_description(self):
-        return self.description
-
-    def get_employer(self):
-        return self.employer
-
-    def get_location(self):
-        return self.location
-
-    def get_salary(self):
-        return self.salary
-
-    def get_post_name(self):
-        return self.post_name
-=======
 class Job:
     def __init__(self, title, description, employer, location, salary, poster_name):
         self.title = title
@@ -52,4 +24,4 @@ class Job:
 
     def get_poster_name(self):
         return self.poster_name
->>>>>>> Epic6
+
